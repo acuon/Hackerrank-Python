3924fe778b79648ba830e169051ae8668a185960
acuon <83219534+acuon@users.noreply.github.com>
2021-05-05 10:51:45 +0530

    function to find leap years

--- WriteaFunction.py
+++ WriteaFunction.py
@@ -1,5 +1,11 @@
 #https://www.hackerrank.com/challenges/write-a-function/problem
 
+#In the Gregorian calendar, three conditions are used to identify leap years:
+#    The year can be evenly divided by 4, is a leap year, unless:
+#        The year can be evenly divided by 100, it is NOT a leap year, unless:
+#            The year is also evenly divisible by 400. Then it is a leap year.
+
+
 import calendar
 def is_leap(year):
     leap = calendar.isleap(year)
