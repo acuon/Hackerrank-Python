901ba781bda668b51c0c3fa8931293be2810ca50
acuon <83219534+acuon@users.noreply.github.com>
2021-05-05 10:57:14 +0530

    Create PrintFunction.py
    to print the list of integers as a string without spaces

---
+++ PrintFunction.py
@@ -0,0 +1,14 @@
+#https://www.hackerrank.com/challenges/python-print/problem
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+
+#s=""
+#for i in range(1,n+1):
+#    s=s+str(i)
+#print(s)
+#(OR)
+for i in range(1,n+1):
+    print(i,end="")
